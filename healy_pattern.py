e2e07a95faa0a99e2901f46fbbcfaf043972fd3c
Healy Dwyer <healy_dwyer@csx.com>
2018-12-05 16:33:20 -0500

    change for wildcard

--- healy_pattern.py
+++ healy_pattern.py
@@ -9,16 +9,19 @@ def readFile(file):
     pathArray = []
     count = 0
     arraylength = 0
+    # sets first list length equal to first integer
     for line in file:
         if count < 1:
             arraylength = line
             count = count + 1
+        # filters lists into two separate arrays to compare against each other
         else:
             if len(patternArray) < int(arraylength):
                 patternArray.append(list(filter(None, line.replace('\n', '').split(','))))
             else:
                 pathArray.append(list(filter(None, line.replace('\n', '').split('/'))))
-
+    
+    # removes first integer from second list since it just signifies number of patterns
     pathArray = pathArray[1:]
 
     getMatchingPatterns(patternArray, pathArray)
@@ -26,14 +29,18 @@ def readFile(file):
 def getMatchingPatterns(patternArray, pathArray):
     delimeter = ","
     matches = []
+    #compares each pattern to each path
     for paths in pathArray:
         matches = []
         for pattern in patternArray:
+            # first check if lengths match
             if (len(paths) == len(pattern)):
                 charMatches = 0
+                # if lengths match check each character individually, add 1 each time a character matches
                 for index,char in enumerate(paths):
                     if pattern[index] == char or pattern[index] == '*':
                         charMatches = charMatches + 1
+                        # if the length of the path matches the number of characters that matched, mark as a match
                         if len(paths) == charMatches:
                             matches.append(delimeter.join(pattern))
         if len(matches) == 0:
@@ -48,29 +55,32 @@ def groupMatches(matches):
     lowestCount = None
     currentCount = None
     patternArray = []
+    # in new array of potential matches to the path, compare number of wildcards in each match
     for match in matches:
         currentCount = match.count("*")
+        # initialize empty pattern array, add pattern(s) with lowest amount of wildcards to it 
         if lowestCount > currentCount or lowestCount == None:
             patternArray = []
             lowestCount = currentCount
             patternArray.append(match)
         elif lowestCount == currentCount:
             patternArray.append(match)
-    if len(patternArray) > 1:
+    # if there are multiple matches with equal numbers of wildcards, go on to check positioning of them
+    if len(patternArray) > 1 and lowestCount != 0:
         wildcardMatch(patternArray)
     else:
         print patternArray[0]
 
 def wildcardMatch(patternArray):
-    lowestIndex = None
-    currentIndex = None
-    for pattern in patternArray:
-        try:
-            currentIndex = re.search(r'[^\*]', pattern).start()
-            if lowestIndex < currentIndex or lowestIndex == None:
-                lowestIndex = currentIndex
-        except:
-            lowestIndex = 0
-    print patternArray[lowestIndex]
+    highestindex = None
+    currentindex = None
+    bestpattern = []
+    for index, pattern in enumerate(patternArray):
+        firstwildcard = pattern.index('*')
+        if highestindex == None or highestindex < firstwildcard:
+            highestindex = firstwildcard
+            bestpattern = index
+ 
+    print patternArray[bestpattern]
             
 readFile(file_name)
